a41614e0f7004bd6cf096f3b068dd62edcbd58c3
Youri Dudock <youri@icatt.nl>
2025-09-10 14:21:48 +0200

    temp commit

---
+++ Datamigratie.Common/Services/OpenZaak/OpenZaakApiClient.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using Datamigratie.Common.Services.Det.Models;
+using Datamigratie.Common.Services.OpenZaak.Models.Models;
+using Datamigratie.Common.Services.Shared;
+using Datamigratie.Common.Services.Shared.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Datamigratie.Common.Services.Det
+{
+    public interface IOpenZaakApiClient
+    {
+
+    }
+
+    public class OpenZaakClient(
+        HttpClient httpClient,
+        ILogger<OpenZaakClient> logger) : PagedApiClient(httpClient), IOpenZaakApiClient
+    {
+
+        private readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        /// <summary>
+        /// Gets all zaaktypen with pagination details.
+        /// Endpoint: /zaaktypen
+        /// </summary>
+        /// <returns>A PagedResponse object containing a list of all Zaaktype objects across all pages.</returns>
+        public async Task<List<OzZaaktype>> GetAllZaakTypen()
+        {
+            var pagedZaaktypen = await GetAllPagedData<OzZaaktype>("zaaktypen");
+            return pagedZaaktypen.Results;
+        }
+
+        /// <summary>
+        /// Gets a specific zaaktype by its name.
+        /// Endpoint: /zaaktypen/{name}
+        /// </summary>
+        /// <returns>A zaaktype object, or null if not found</returns>
+        public async Task<DetZaaktype> GetSpecificZaaktype(string zaaktypeName)
+        {
+            var endpoint = $"zaaktypen/{zaaktypeName}";
+            var response = await httpClient.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<DetZaaktype>(jsonString, _options);
+        }
+    }
+
+    }
