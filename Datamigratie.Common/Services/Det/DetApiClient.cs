a68351d6c747efaf5f31f4f9d670503fe5d68762
Youri Dudock <youri@icatt.nl>
2025-10-01 11:39:40 +0200

    fixed imports

--- Datamigratie.Common/Services/Det/DetApiClient.cs
+++ Datamigratie.Common/Services/Det/DetApiClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json;
 using Datamigratie.Common.Extensions;
 using Datamigratie.Common.Services.Det.Models;
