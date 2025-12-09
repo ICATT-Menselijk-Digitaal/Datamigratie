186ca75c8df756f805e5f0b2d5c03460c95cb2f0
ismail ozgen <ismail@info.nl>
2025-12-09 13:25:30 +0100

    increase token issued time from 1 minute to 10 minutes for better clock synchronization

--- Datamigratie.Common/Services/OpenZaak/OpenZaakTokenProvider.cs
+++ Datamigratie.Common/Services/OpenZaak/OpenZaakTokenProvider.cs
@@ -11,7 +11,7 @@ namespace Datamigratie.Common.Services.OpenZaak
         {
             var now = DateTimeOffset.UtcNow;
             // one minute leeway to account for clock differences between machines
-            var issuedAt = now.AddMinutes(-1);
+            var issuedAt = now.AddMinutes(-10);
             var iat = issuedAt.ToUnixTimeSeconds();
 
             var claims = new Dictionary<string, object>
