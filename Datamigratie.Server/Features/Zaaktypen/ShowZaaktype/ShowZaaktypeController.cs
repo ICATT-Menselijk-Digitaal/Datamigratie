f8a4dd5e8c5ca4c21d91dbdfb9c626de2dfdba2b
Youri Dudock <youri@icatt.nl>
2026-01-08 14:00:12 +0100

    refactored zaaktype controller feature

--- Datamigratie.Server/Features/Zaaktypen/ShowZaaktype/ShowZaaktypeController.cs
+++ Datamigratie.Server/Features/Zaaktypen/ShowZaaktype/ShowZaaktypeController.cs
@@ -1,3 +1,4 @@
+﻿using Datamigratie.Common.Services.OpenZaak;
 using Datamigratie.Server.Features.Zaaktypen.ShowZaaktype.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,12 +6,26 @@ namespace Datamigratie.Server.Features.Zaaktypen.ShowZaaktype
 {
     [ApiController]
     [Route("api/oz/zaaktypen")]
-    public class ShowZaaktypeController(IShowZaaktypeService showZaaktypeService) : ControllerBase
+    public class ShowZaaktypeController(IOpenZaakApiClient openZaakApiClient) : ControllerBase
     {
         [HttpGet("{zaaktypeId}")]
         public async Task<ActionResult<EnrichedOzZaaktype>> GetEnrichedZaaktype(Guid zaaktypeId)
         {
-            var enrichedOzZaaktype = await showZaaktypeService.GetEnrichedZaaktype(zaaktypeId);
+            var ozZaaktype = await openZaakApiClient.GetZaaktype(zaaktypeId);
+
+            if (ozZaaktype == null)
+            {
+                return NotFound($"OZ Zaaktype is not found with id {zaaktypeId}");
+            }
+
+            var ozResultaattypen = await openZaakApiClient.GetResultaattypenForZaaktype(zaaktypeId);
+
+            var enrichedOzZaaktype = new EnrichedOzZaaktype
+            {
+                Url = ozZaaktype.Url,
+                Identificatie = ozZaaktype.Identificatie,
+                Resultaattypen = ozResultaattypen,
+            };
 
             return enrichedOzZaaktype == null ? NotFound() : enrichedOzZaaktype;
         }
