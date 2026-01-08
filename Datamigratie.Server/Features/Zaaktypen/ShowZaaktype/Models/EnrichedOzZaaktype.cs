b9b412260ece9e3ba5e675877202888279b36bbc
Youri Dudock <youri@icatt.nl>
2026-01-08 14:19:59 +0100

    removed enrichedOzzaaktype

--- Datamigratie.Server/Features/Zaaktypen/ShowZaaktype/Models/EnrichedOzZaaktype.cs
+++
@@ -1,9 +0,0 @@
-using Datamigratie.Common.Services.OpenZaak.Models;
-
-namespace Datamigratie.Server.Features.Zaaktypen.ShowZaaktype.Models
-{
-    public class EnrichedOzZaaktype : OzZaaktype
-    {
-        public List<OzResultaattype>? Resultaattypen { get; set; }
-    }
-}
