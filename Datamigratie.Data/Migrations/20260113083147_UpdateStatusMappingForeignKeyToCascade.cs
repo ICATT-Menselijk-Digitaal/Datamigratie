5969189e05c39f7025a99d0470990c4831f80ac9
ismail ozgen <ismail@info.nl>
2026-01-13 09:35:33 +0100

    Update foreign key relationship for StatusMappings to use cascade delete

---
+++ Datamigratie.Data/Migrations/20260113083147_UpdateStatusMappingForeignKeyToCascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datamigratie.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStatusMappingForeignKeyToCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StatusMappings_Mappings_ZaaktypenMappingId",
+                table: "StatusMappings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StatusMappings_Mappings_ZaaktypenMappingId",
+                table: "StatusMappings",
+                column: "ZaaktypenMappingId",
+                principalTable: "Mappings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StatusMappings_Mappings_ZaaktypenMappingId",
+                table: "StatusMappings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StatusMappings_Mappings_ZaaktypenMappingId",
+                table: "StatusMappings",
+                column: "ZaaktypenMappingId",
+                principalTable: "Mappings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
