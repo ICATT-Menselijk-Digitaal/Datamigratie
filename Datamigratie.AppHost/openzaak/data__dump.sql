2435000cde6432eefd50938c6919ae39fec793d9
Felix Cornelissen <felix@icatt.nl>
2026-01-15 16:10:42 +0100

    wip

--- Datamigratie.AppHost/openzaak/data__dump.sql
+++ Datamigratie.AppHost/openzaak/data__dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3587421bdbd601f813a6789351571c78aa1761d0fb4a9b9d0a8f07881cf8c3fa
+oid sha256:e6dfa69022c2be522e86e314d79ce4f91c307f7bcb046f9fb32c04aaf4d3cd00
 size 2107326
