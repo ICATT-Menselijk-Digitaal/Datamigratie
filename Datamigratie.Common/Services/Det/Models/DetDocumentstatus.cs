e577d715946a92517a8925f76f65f4e3cf265b08
Youri Dudock <youri@icatt.nl>
2026-01-21 15:17:49 +0100

    reverted document status

--- Datamigratie.Common/Services/Det/Models/DetDocumentstatus.cs
+++ Datamigratie.Common/Services/Det/Models/DetDocumentstatus.cs
@@ -1,7 +1,9 @@
-namespace Datamigratie.Common.Services.Det.Models
+﻿namespace Datamigratie.Common.Services.Det.Models
 {
     public class DetDocumentstatus
     {
+        public bool Actief { get; set; }
         public required string Naam { get; set; }
+        public string? Omschrijving { get; set; }
     }
 }
