b1e4cba88693d114fcdc951d0dc9d423a07064c3
Youri Dudock <youri@icatt.nl>
2026-01-29 11:01:27 +0100

    re-added migration files for documentstatus

---
+++ Datamigratie.Data/Migrations/20260129095741_AddDocumentstatusMapping.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Datamigratie.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentstatusMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DocumentstatusMappings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DetDocumentstatus = table.Column<string>(type: "text", nullable: false),
+                    OzDocumentstatus = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentstatusMappings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentstatusMapping_DetDocumentstatus_Unique",
+                table: "DocumentstatusMappings",
+                column: "DetDocumentstatus",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentstatusMappings");
+        }
+    }
+}
