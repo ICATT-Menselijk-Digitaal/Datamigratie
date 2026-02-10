88d342df136f9dc6e41078af185faa036075d98c
Youri Dudock <youri@icatt.nl>
2026-02-10 14:53:27 +0100

    changed to required

--- Datamigratie.Common/Services/OpenZaak/Models/OzBesluit.cs
+++ Datamigratie.Common/Services/OpenZaak/Models/OzBesluit.cs
@@ -68,7 +68,7 @@ namespace Datamigratie.Common.Services.OpenZaak.Models
         /// De omschrijving die aangeeft op grond waarvan het besluit is of komt te vervallen.
         /// </summary>
         [JsonPropertyName("vervalreden")]
-        public Vervalreden Vervalreden { get; set; }
+        public required Vervalreden Vervalreden { get; set; }
 
         /// <summary>
         /// Datum waarop het besluit gepubliceerd wordt.
@@ -122,7 +122,7 @@ namespace Datamigratie.Common.Services.OpenZaak.Models
         public DateOnly? Vervaldatum { get; set; }
 
         [JsonPropertyName("vervalreden")]
-        public Vervalreden Vervalreden { get; set; }
+        public required Vervalreden Vervalreden { get; set; }
 
         [JsonPropertyName("publicatiedatum")]
         public DateOnly? Publicatiedatum { get; set; }
