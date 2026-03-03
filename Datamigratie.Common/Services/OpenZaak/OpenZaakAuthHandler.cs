bb025cbdf05ec0d19dece9329ae53de10591e738
Youri Dudock <youri@icatt.nl>
2026-03-03 11:22:02 +0100

    Always get new JWT token

--- Datamigratie.Common/Services/OpenZaak/OpenZaakAuthHandler.cs
+++ Datamigratie.Common/Services/OpenZaak/OpenZaakAuthHandler.cs
@@ -7,38 +7,13 @@ namespace Datamigratie.Common.Services.OpenZaak;
 
 internal class OpenZaakAuthHandler(IOptions<OpenZaakApiOptions> options) : DelegatingHandler
 {
-    // Token lifetime: 1 hour minus 1-min clock leeway; refresh 1m before expiry = 58 min
-    private static readonly TimeSpan s_tokenLifetime = TimeSpan.FromMinutes(58);
-
-    private readonly OpenZaakApiOptions _options = options.Value;
-    private string? _cachedToken;
-    private DateTime _tokenExpiresAt = DateTime.MinValue;
-
     protected override async Task<HttpResponseMessage> SendAsync(
         HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", GetOrRefreshToken());
+        var token = OpenZaakTokenProvider.GenerateZakenApiToken(options.Value.ApiKey, options.Value.ApiUser);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
         var response = await base.SendAsync(request, cancellationToken);
-
-        if (response.StatusCode == HttpStatusCode.Forbidden)
-        {
-            _tokenExpiresAt = DateTime.MinValue; // force regeneration
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", GetOrRefreshToken());
-            response.Dispose();
-            response = await base.SendAsync(request, cancellationToken);
-        }
-
         return response;
-    }
-
-    private string GetOrRefreshToken()
-    {
-        if (_cachedToken == null || DateTime.UtcNow >= _tokenExpiresAt)
-        {
-            _cachedToken = OpenZaakTokenProvider.GenerateZakenApiToken(_options.ApiKey, _options.ApiUser);
-            _tokenExpiresAt = DateTime.UtcNow.Add(s_tokenLifetime);
-        }
-        return _cachedToken;
     }
 }
