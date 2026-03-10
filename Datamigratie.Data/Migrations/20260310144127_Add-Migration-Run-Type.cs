bdc25efee355cb945c9ebf4d916c2cedd4c0440e
Youri Dudock <youri@icatt.nl>
2026-03-10 15:52:50 +0100

    migration

---
+++ Datamigratie.Data/Migrations/20260310144127_Add-Migration-Run-Type.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datamigratie.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigrationRunType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MigrationType",
+                table: "Migrations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MigrationType",
+                table: "Migrations");
+        }
+    }
+}
