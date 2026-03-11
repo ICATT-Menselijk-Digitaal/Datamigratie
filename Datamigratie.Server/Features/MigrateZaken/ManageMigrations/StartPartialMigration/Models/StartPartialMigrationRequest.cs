d234c46e5ed704b1fa494efdc3494e5a7c47d274
Youri Dudock <youri@icatt.nl>
2026-03-11 09:18:59 +0100

    readded required

--- Datamigratie.Server/Features/MigrateZaken/ManageMigrations/StartPartialMigration/Models/StartPartialMigrationRequest.cs
+++ Datamigratie.Server/Features/MigrateZaken/ManageMigrations/StartPartialMigration/Models/StartPartialMigrationRequest.cs
@@ -1,6 +1,9 @@
-﻿namespace Datamigratie.Server.Features.MigrateZaken.ManageMigrations.StartPartialMigration.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Datamigratie.Server.Features.MigrateZaken.ManageMigrations.StartPartialMigration.Models;
 
 public class StartPartialMigrationRequest
 {
+    [Required]
     public required string DetZaaktypeId { get; set; }
 }
