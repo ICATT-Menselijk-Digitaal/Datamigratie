f93d110f1dafd031eb4fad6f6a67e1f3103a26a9
Youri Dudock <youri@icatt.nl>
2026-03-11 09:18:15 +0100

    removed data annotation

--- Datamigratie.Server/Features/MigrateZaken/ManageMigrations/StartPartialMigration/Models/StartPartialMigrationRequest.cs
+++ Datamigratie.Server/Features/MigrateZaken/ManageMigrations/StartPartialMigration/Models/StartPartialMigrationRequest.cs
@@ -1,9 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Datamigratie.Server.Features.MigrateZaken.ManageMigrations.StartPartialMigration.Models;
+﻿namespace Datamigratie.Server.Features.MigrateZaken.ManageMigrations.StartPartialMigration.Models;
 
 public class StartPartialMigrationRequest
 {
-    [Required]
     public required string DetZaaktypeId { get; set; }
 }
